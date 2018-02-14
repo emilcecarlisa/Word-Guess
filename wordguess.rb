7bd8cb0d1182a61dfd86222a6eda2920cc589796
emilcecarlisa <emilcearlisa@gmail.com>
2018-02-13 20:58:34 -0800

    Added Art class and used in the Word class. Added conditional statement for storing the incorrect guesses in an array

--- wordguess.rb
+++ wordguess.rb
@@ -3,80 +3,111 @@
 # 12th - 14th Feb 2018
 # https://github.com/Ada-C9/Word-Guess
 
-
 class Art
-  # where we create the ASC art
-  attr_reader
+  attr_accessor :art
 
-  def initialize()
+  def initialize
+    @art = art
+  end
 
+  def show_art
+     show_art = "(=^ ◡ ^=)"
+     return show_art
+  end
+
+  def art_array
+    art_array = show_art.split("")
+    puts "#{art_array}"
+    return art_array
   end
 
 end
 
 class Word
-  attr_accessor :guess
+  attr_accessor :guess_letter
+  attr_reader :selection_of_word, :create_underscore_array
 
   def initialize
     @word = selection_of_word
-    @guess_array = build_guess_array
-  end
+    @replaced_underscore = create_underscore_array
+    @guesses_available = 9
 
-  def guess_status
-    return @guess_array.join("")
   end
 
   def selection_of_word()
     # body parts variable is set to an array of strings representing body parts
-    words = ["hand", "forearm", "upperarm", "surgery", "limbless", "gore", "shrug", "man", "knife", "amputation"]
+    words = ["kitty", "paw", "litter", "leash", "tongue", "grumpy", "fierce", "tail", "whiskers", "claws"]
     # randomly grabbing a word from body_parts array
     random_word = words.sample
 
   end
 
-  def build_guess_array
+  def create_underscore_array
     random_word_length = @word.length
-    # dashes = "_" * random_word_length # user sees this
-    # with every correct guess a dash is removed
-    # and replaced with a letter
     puts @word
 
-    guess_array = Array.new(random_word_length, "_ ")
+    underscore_array = Array.new(random_word_length, "-")
 
     # putting contents of 'guess' using the join method (makes underscores print horizontally)
-    puts guess_array.join("")
-    # return guess_array.join("")
+    # user sees this
+    puts underscore_array.join("")
+    return underscore_array.join("")
   end
 
-  def guess_letter(letter) # we split the letter in here for comparison against user input
-    # user_letter = guess # what is this SUPPOSED to be doing?
+  def guess_letter
+    art = Art.new
+    word_characters = @word.chars
 
-    # splitting 'random_word' string into its individual characters and storing them into the 'random_word_array' array
-    random_word_array = random_word.chars
+    user_guess_count = 0
+    incorrect_guesses = []
 
-    # get the user guess
-    puts "Provide a letter"
-    guess = gets.chomp.downcase
+    while user_guess_count > 0 && @replaced_underscore.include?("-")
+      puts art.show_art
+      puts art.art_array
+      puts "Provide a letter"
+      guess = gets.chomp.downcase
 
-    # COMPARE user guess to the split array
-    # random_word_array looped through index
-    random_word_array.each_index do |index|
-      # putting the current index equal to letter so we can use a conditional
-      letter = random_word_array[index]
-      # if letter is equal to the letter user submitted...
-      if letter == guess
-        # guess[index] = user_letter
-        # **in this case below, the 'guess[index]' on the left is location, the 'guess[index]' is the dash being replaced by the user_letter
-        ###doing the replacement here
-        @guess_array[index] = guess_array[index].sub("_", user_letter)
-        # used for testing to check output
-        puts guess.join("")
-        puts index
-        puts user_letter
+      # COMPARE user guess to the split array
+      word_characters.each_index do |index|
+        # putting the current index equal to letter so we can use a conditional
+        letter_of_random_word = word_characters[index]
+        if letter_of_random_word == guess
+          # @replaced_underscore[index] = @replaced_underscore[index].sub("_", guess)
+          @replaced_underscore[index] = letter_of_random_word
+          # puts replaced_underscore.join("")
+          puts @replaced_underscore
+        else
+          guesses_available -= 1
+          puts "Guesses_left: #{guesses_available}"
+          incorrect_guesses << guess
+
+        end
       end
+
+      # if @replaced_underscore[index] != letter_of_random_word
+      #   incorrect_guesses << guess
+      # end
+      #
+      # puts "#{incorrect_guesses}"
+      puts "Not finished, keep going"
+
+      # user_guess_count += 1
+      # puts "Guesses left: #{@guesses_available - user_guess_count}"
+      #
+      # if user_guess_count == @guesses_available
+      #   puts "You're out of guesses!"
+      # end
     end
+
+    puts "You win!"
+    exit
   end
 end
 
+# count the number of guesses inside of the word Array
+# the number of
+
+
 attempt = Word.new # this runs the whole thing once
-# puts attempt.build_guess_array # this runs the program again(and with a specific behavior invoked), but same word
+# puts attempt.create_underscore_array # this runs the program again(and with a specific behavior invoked), but same word
+attempt.guess_letter
